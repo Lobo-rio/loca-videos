668d1678d4e1036c8b8e3ab6b51ac5b8dd4efc82
Lobo-rio <gsilvamedeiros@gmail.com>
2023-06-26 11:43:08 -0300

    feat: update files and folders find by id usecase, test to actors

---
+++ src/domain/movie-stores/application/use-cases/actors/find-by-id-actorusecase.ts
@@ -0,0 +1,20 @@
+import { Either, left, right } from '@/core/types/either'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+import { Actors } from '@/domain/movie-stores/enterprise/entities/actors/actors'
+import { ActorsRepository } from '../../repositories/actors/actors-repository'
+
+type FindByIdActorsUseCaseResponse = Either<ResourceNotFoundError, {
+  actor: Actors
+}>
+
+export class FindByIdActorsUseCase {
+  constructor(private readonly actorRepository: ActorsRepository) {}
+
+  async execute(id: string): Promise<FindByIdActorsUseCaseResponse> {
+    const actor = await this.actorRepository.findById(id)
+
+    if (!actor) return left(new ResourceNotFoundError())
+
+    return right({ actor })
+  }
+}
