d8008804331f8deb047efc2fe3aa28c68065db32
Lobo-rio <gsilvamedeiros@gmail.com>
2023-06-26 11:52:08 -0300

    feat: update files and folders find by many usecase, test to actors

---
+++ src/domain/movie-stores/application/use-cases/actors/find-many-actor-usecase.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryActorsRepository } from "test/repositories/in-memory-actors-repository"
+import { CreateActorsUseCase } from "./create-actors-usecase"
+import { FindManyActorsUseCase } from "./find-many-actor-usecase"
+import { makeActors } from "test/factories/make-actors"
+
+let inMemoryActorsRepository: InMemoryActorsRepository
+let createActorsUseCase: CreateActorsUseCase
+let sut: FindManyActorsUseCase
+
+describe('Find Many Actors', () => {
+  beforeEach(() => {
+    inMemoryActorsRepository = new InMemoryActorsRepository()
+    createActorsUseCase = new CreateActorsUseCase(inMemoryActorsRepository)
+    sut = new FindManyActorsUseCase(inMemoryActorsRepository)
+  })
+
+  it('should be able to find many a actors', async () => {
+    const newActor = makeActors()
+    await createActorsUseCase.execute(newActor)
+
+    const result = await sut.execute()
+
+    expect(result.isRight()).toBe(true)
+  })
+})
---
+++ src/domain/movie-stores/application/use-cases/actors/find-many-actor-usecase.ts
@@ -0,0 +1,19 @@
+import { Either, right } from '@/core/types/either'
+import { Actors } from '@/domain/movie-stores/enterprise/entities/actors/actors'
+import { ActorsRepository } from '../../repositories/actors/actors-repository'
+
+type FindManyActorsUseCaseResponse = Either<null, {
+  actors: Actors[]
+}>
+
+export class FindManyActorsUseCase {
+  constructor(private readonly actorRepository: ActorsRepository) {}
+
+  async execute(): Promise<FindManyActorsUseCaseResponse> {
+    const actors = await this.actorRepository.findMany()
+
+    return right({
+      actors,
+    })
+  }
+}
