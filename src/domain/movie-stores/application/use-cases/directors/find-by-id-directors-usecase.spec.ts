fa15eefcb8567871023deaed752f1bbae5802c11
Lobo-rio <gsilvamedeiros@gmail.com>
2023-06-26 14:19:23 -0300

    feat: update files and folders find by id usecase, test to directors

---
+++ src/domain/movie-stores/application/use-cases/directors/find-by-id-directors-usecase.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryDirectorsRepository } from 'test/repositories/in-memory-directors-repository'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+import { CreateDirectorsUseCase } from './create-directors-usecase'
+import { FindByIdDirectorsUseCase } from './find-by-id-directors-usecase'
+import { makeDirectors } from 'test/factories/make-directors'
+
+let inMemoryDirectorsRepository: InMemoryDirectorsRepository
+let createDirectorsUseCase: CreateDirectorsUseCase
+let sut: FindByIdDirectorsUseCase
+
+describe('Find By Id Director', () => {
+  beforeEach(() => {
+    inMemoryDirectorsRepository = new InMemoryDirectorsRepository()
+    createDirectorsUseCase = new CreateDirectorsUseCase(inMemoryDirectorsRepository)
+    sut = new FindByIdDirectorsUseCase(inMemoryDirectorsRepository)
+  })
+
+  it('should be able to find by id a director', async () => {
+    const newDirector = makeDirectors()
+    const directorCreated = await createDirectorsUseCase.execute(newDirector)
+    let id: string = ''
+    if (directorCreated.isRight()) id = directorCreated.value?.director.id.toString()
+
+    const result = await sut.execute(id)
+
+    expect(result.isRight()).toEqual(true)
+  })
+
+  it('should be able to find by id a director not found', async () => {
+    const newDirector = makeDirectors()
+    await createDirectorsUseCase.execute(newDirector)
+
+    const result = await sut.execute('director-test-1')
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+  })
+})
---
+++ src/domain/movie-stores/application/use-cases/directors/find-by-id-directors-usecase.ts
@@ -0,0 +1,20 @@
+import { Either, left, right } from '@/core/types/either'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+import { Directors } from '@/domain/movie-stores/enterprise/entities/directors/directors'
+import { DirectorsRepository } from '../../repositories/directors/directors-repository'
+
+type FindByIdDirectorsUseCaseResponse = Either<ResourceNotFoundError, {
+  director: Directors
+}>
+
+export class FindByIdDirectorsUseCase {
+  constructor(private readonly directorRepository: DirectorsRepository) {}
+
+  async execute(id: string): Promise<FindByIdDirectorsUseCaseResponse> {
+    const director = await this.directorRepository.findById(id)
+
+    if (!director) return left(new ResourceNotFoundError())
+
+    return right({ director })
+  }
+}
